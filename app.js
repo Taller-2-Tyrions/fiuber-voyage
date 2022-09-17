8de44e8ecb8cc37e6339584adf565c735474bc6f
Lucas Veron <veron_lucas@hotmail.com>
2022-09-17 19:37:31 -0300

    change caller protocol

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var PAYMENT_URI = process.env.PAYMENT_URI
 var PAYMENT_HOST = process.env.PAYMENT_HOST
 var PAYMENT_PORT = process.env.PAYMENT_PORT
 var PAYMENT_PATH = process.env.PAYMENT_PATH
-const PAYMENT_PROTOCOL = process.env.PAYMENT_PROTOCOL
 const SRV_PORT = process.env.SRV_PORT
 
 const express = require("express");
 const request = require("request");
 const http = require("http");
+const https = require("https");
 //var cors = require('cors')
 
 const fetch = (...args) =>
@@ -25,7 +25,6 @@ app.get("/contents/:idUser", async (req,res) => {
   console.log("Retrieve utility content for user id: "+ idUser)
 
   var options = {
-    protocol: PAYMENT_PROTOCOL,
     host: PAYMENT_HOST,
     port: PAYMENT_PORT,
     path: PAYMENT_PATH+'/3'
@@ -34,13 +33,13 @@ app.get("/contents/:idUser", async (req,res) => {
   console.log("Request fiuber-payments uri: "+ options.host+":"+options.port+"/"+options.path)
   
 
-  http.get(options, function(response) {
+  https.get(options, function(response) {
     console.log("Response status code: " + response.statusCode);
 
     response.on("data", function(chunk) {
 
       console.log("Body: " + chunk);
-      //let utilityContent = {score: 5}
+      
       res.set(CONTENT_TYPE_JSON)
       res.send(chunk)
       
