50a62e24ead6e8a1a3593656eb94106435fd2256
Lucas Veron <veron_lucas@hotmail.com>
2022-09-19 16:33:21 -0300

    add db impl travels

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var PAYMENT_PORT = process.env.PAYMENT_PORT
 var PAYMENT_PATH = process.env.PAYMENT_PATH
 const SRV_PORT = process.env.SRV_PORT
 
+const DATABASE_URL = process.env.PG_DATABASE_URL
+
 const express = require("express");
 const request = require("request");
 const http = require("http");
@@ -18,6 +20,34 @@ const app = express();
 
 const CONTENT_TYPE_JSON = {'Content-Type': 'application/json' }
 const EMPTY_JSON = "{}"
+
+const DATE_FORMAT = "YYYY-MM-DD HH:mm:ss"
+const SELECT_TRAVELS_FOR_ID_USER = "SELECT * FROM public.travels WHERE user_id = $1 ORDER BY creation_date DESC"
+
+var pgp = require("pg-promise")({
+   capSQL: true
+});//(/*options*/)
+var db = pgp(DATABASE_URL);
+
+app.get("/activity/:idUser/travels/:travelCount", async (req, res) =>{
+  let travelCount = req.params.travelCount
+  let idUser = req.params.idUser
+
+  console.log("Retrieve activity content for user id: "+ idUser+",travelCount: ",travelCount)
+
+  let res_data = await db.one(SELECT_TRAVELS_FOR_ID_USER, idUser)
+      .then(data => {
+        console.log("Travels:", data)
+        return JSON.stringify(data)
+      })
+      .catch(function (error) {
+          console.log("Error: retrieve Travels ", error)
+          return EMPTY_JSON
+      })
+  res.set(CONTENT_TYPE_JSON)
+  res.send(res_data)
+
+})
 
 app.get("/contents/:idUser", async (req,res) => {
   var idUser  = req.params.idUser
