22ee320e34496cde1dab367e0660b34c27c76498
mateocapon <mcapon@fi.uba.ar>
2022-10-17 20:45:06 -0300

    primera push notif

--- app/firebase_notif/firebase.py
+++ app/firebase_notif/firebase.py
@@ -1,7 +1,16 @@
-from ..schemas.voyage import InitVoyageBase
+from firebase_admin import db as firebase_db
+from exponent_server_sdk import (
+    DeviceNotRegisteredError,
+    PushClient,
+    PushMessage,
+    PushServerError,
+    PushTicketError,
+)
 
 
-def notify_drivers(near_drivers, voyage: InitVoyageBase):
-    # To do
-    print("Aca se envia notificacion")
-    return 0
+
+def notify(user_id, message):
+    ref = firebase_db.reference("/"+user_id)
+    device_token = ref.get()
+    mes = PushMessage(to=device_token["token"], body=message)
+    PushClient().publish(mes)
