f31b3aaefa0d234a2e06ace598e4a3d1d48774fb
niragui <ignacioiragu2@gmail.com>
2022-11-17 03:32:18 -0300

    Arreglos Parciales
    Co-authored-by: Mateo Capon Blanquer <mateocapon@users.noreply.github.com>

--- app/schemas/pricing.py
+++ app/schemas/pricing.py
@@ -1,24 +1,33 @@
 from pydantic import BaseModel
 from .common import Point
+from typing import List
 
 
-class PriceRequest(BaseModel):
-    origin: Point
-    destination: Point
-    distance: float
+class PriceUserBase(BaseModel):
+    seniority: int
+    day_voyages: int
+    month_voyages: int
+
+
+class PriceVoyageBase(BaseModel):
+    init: Point
+    end: Point
     is_vip: bool
 
 
-class PriceResponse(BaseModel):
-    price: float
+class PriceDriverBase(PriceUserBase):
+    id: str
+    location: Point
+    is_vip: bool
 
 
-class UserBase(BaseModel):
-    day_voyages: int
-    month_voyages: int
-    seniority: int
+class PriceRequestBase(BaseModel):
+    voyage: PriceVoyageBase
+    passenger: PriceUserBase
+    driver: PriceDriverBase
 
 
-class ClientBase(UserBase):
-    money: int
-    payment_method: str
+class PriceRequestsBase(BaseModel):
+    voyage: PriceVoyageBase
+    passenger: PriceUserBase
+    drivers: List[PriceDriverBase]
